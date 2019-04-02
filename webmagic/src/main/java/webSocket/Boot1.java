1461c0553222bfb9702a5ab31429e54a7eb2eb86
auguszero <auguszero@163.com>
2019-04-02 16:27:48 +0800

    32
    42

--- webmagic/src/main/java/webSocket/Boot1.java
+++ webmagic/src/main/java/webSocket/Boot1.java
@@ -15,7 +15,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class Boot1 {
 
-    public static boolean flage = false;
+    public static boolean flage = true;
     public static int index = 0;
     public static ReentrantLock reentrantLock = new ReentrantLock();
 
@@ -26,9 +26,11 @@ public class Boot1 {
         int args4 = Integer.parseInt(args[3]);
         int args5 = Integer.parseInt(args[4]);
         int args6 = Integer.parseInt(args[5]);
-        int num = Integer.parseInt(args[6]);
-        KeyAd keyAd = new KeyAd();
-        buttonInsert(args1, args2, args3, args4, args5, args6, num);
+        int args7 = Integer.parseInt(args[6]);
+        int num = Integer.parseInt(args[7]);
+//        KeyAd keyAd = new KeyAd();
+        buttonInsert(args1, args2, args3, args4, args5, args6,args7 ,num);
+        System.out.println(1);
 
     }
 
@@ -42,7 +44,7 @@ public class Boot1 {
      * @param args5
      * @param args6
      */
-    private static void buttonInsert(int args1, int args2, int args3, int args4, int args5, int args6, int num) {
+    private static void buttonInsert(int args1, int args2, int args3, int args4, int args5, int args6, int args7, int num) {
         Robot robot = null;
         try {
             robot = new Robot();
@@ -57,15 +59,15 @@ public class Boot1 {
                     robot.mouseMove(args1, args2);
                     robot.mousePress(InputEvent.BUTTON1_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                    Thread.sleep(25);
+                    Thread.sleep(args7);
                     robot.mouseMove(args3, args4);
                     robot.mousePress(InputEvent.BUTTON1_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                    Thread.sleep(25);
+                    Thread.sleep(args7);
                     robot.mouseMove(args5, args6);
                     robot.mousePress(InputEvent.BUTTON1_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                    Thread.sleep(25);
+                    Thread.sleep(args7);
                 }
             }
         } catch (Exception e) {
